6d14611cb80b2ddf576b0eb4249f70fb00743514
Dagwaii <165543424+Dagwaii@users.noreply.github.com>
2024-12-25 12:35:56 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -450,6 +450,9 @@ function touchStarted() {
   if (d > 50*lpScale && d < 170*lpScale) {
     changeType(1); // 스타일 변경
     }
+  else{
+     return false;
+  }
   
   // LP 회전 영역 클릭 및 드래그 시작
  if (isInsideLP(touches[0].x, touches[0].y)) {
